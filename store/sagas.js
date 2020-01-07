ec3da13219dfda63d5f5bd984547ca581715199d
JulyQqq <1552906556@qq.com>
2020-01-07 13:41:28 +0800

    2 files modified

--- store/sagas.js
+++ store/sagas.js
@@ -3,7 +3,7 @@ import { GET_INIT_LIST } from "./actionTypes";
 import axios from "axios";
 import { initListAction } from "./actionCreators";
 
-function* getInitList() {
+function getInitList() {
   // try {
   //   console.log("abc");
   //   const res = yield axios.get("/xx.json");
@@ -15,10 +15,19 @@ function* getInitList() {
   //   const action = initListAction(list);
   //   yield put(action);
   // }
-  console.log("abc");
-  const list = ["jack", "rose"];
-  const action = initListAction(list);
-  yield put(action);
+  // console.log("abc");
+  // const list = ["jack", "rose"];
+  // const action = initListAction(list);
+  // yield put(action);
+  const url = 'https://route.showapi.com/341-2?maxResult=20&page=1&showapi_appid=135860&showapi_timestamp=20200107133628&showapi_sign=15e05ffa14789495adb25cac61404770'
+  axios.get(url).then(res => {
+    if (res.data.showapi_res_code === 0) {
+      const list = res.data.showapi_res_body.contentlist
+      console.log(list)
+      const action = initListAction(list)
+      put(action)
+    }
+  });
 }
 function* mySaga() {
   yield takeEvery(GET_INIT_LIST, getInitList);
