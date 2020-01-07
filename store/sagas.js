e5cc0603140a73670a976f843f736696adf06521
JulyQqq <1552906556@qq.com>
2020-01-07 14:51:10 +0800

    2 files modified

--- store/sagas.js
+++ store/sagas.js
@@ -1,28 +1,29 @@
 import { takeEvery, put } from "redux-saga/effects";
-import { GET_INIT_LIST } from "./actionTypes";
+import { INIT_LIST } from "./actionTypes";
 import axios from "axios";
 import { initListAction } from "./actionCreators";
 
 function* getInitList() {
-  // const url =
-  //   "https://route.showapi.com/341-2?maxResult=20&page=1&showapi_appid=135860&showapi_timestamp=20200107133628&showapi_sign=15e05ffa14789495adb25cac61404770";
-  // try {
-  //   console.log("abc");
-  //   const res = yield axios.get(url);
-  //   const list = res.data.showapi_res_body.contentlist;
-  //   console.log(list);
-  //   const action = initListAction(list);
-  //   yield put(action);
-  // } catch (e) {
-  //   console.log("e");
-  //   const list = ["jack", "rose"];
-  //   const action = initListAction(list);
-  //   yield put(action);
-  // }
-  console.log("abc");
-  const list = ["jack", "rose"];
-  const action = initListAction(list);
-  yield put(action);
+  const url =
+    "https://route.showapi.com/341-2?maxResult=20&page=1&showapi_appid=135860&showapi_timestamp=20200107145003&showapi_sign=92e5b3b8d609bc1a97ba29706228d282";
+  try {
+    const res = yield axios.get(url);
+    const list = res.data.showapi_res_body.contentlist;
+    console.log(list);
+    const action = initListAction(list);
+    console.log(action)
+    yield put(action);
+  } catch (e) {
+    console.log("e");
+    const list = ["jack", "rose"];
+    const action = initListAction(list);
+    yield put(action);
+  }
+  // console.log("abc");
+  // const list = ["jack", "rose"];
+  // const action = initListAction(list);
+  // console.log(action)
+  // yield put(action);
 }
 function* mySaga() {
   yield takeEvery(INIT_LIST, getInitList);
